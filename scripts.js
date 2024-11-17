c3aac6ce9dc2b8b14a54fb99f451d86e934c4760
Leon Yang <leonmon3ter@gmail.com>
2024-11-16 20:01:15 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,12 @@
 // scripts.js
 
 window.onload = function() {
-console.log("scripts.js页面加载完成！");
-     console.log("window.onload 已执行");
+    console.log("scripts.js页面加载完成！");
+    console.log("window.onload 已执行");
     alert("欢迎来到杨的个人网站！网站依旧在开发中，不代表最终品质。");
 
     displayRandomQuote();
 };
-
 
 const quotes = [
     "I’ve become your shadow, your misery was mine.",
@@ -17,20 +16,16 @@ const quotes = [
     `I hear it from no one,
     It occur to me the old world,
     It may have said no word,
-    But I have to keep knowing.`
-     "All I see is illusion it’s all illusion it’s all illusion
-     All I am is illusion it’s all illusion it’s all illusion"
-"I’ve seen the highest high
-When I’m down in the lowest low"
-     "I’m out of sight in the darkest night,
-Where my faith is overthrown,
-But I won’t let it reign over my mind."
-     "I’m due for a miracle"
+    But I have to keep knowing.`,
+    "All I see is illusion it’s all illusion it’s all illusion. All I am is illusion it’s all illusion it’s all illusion.",
+    "I’ve seen the highest high when I’m down in the lowest low.",
+    "I’m out of sight in the darkest night, where my faith is overthrown, but I won’t let it reign over my mind.",
+    "I’m due for a miracle"
 ];
 
 // 随机显示句子的函数
 function displayRandomQuote() {
-     console.log("displayRandomQuote 已执行");
+    console.log("displayRandomQuote 已执行");
     const randomIndex = Math.floor(Math.random() * quotes.length); // 随机选择索引
     const randomQuote = quotes[randomIndex]; // 取出随机句子
     document.getElementById("random-quote").innerText = randomQuote;
